05ac2232d11cc38a6135ccd47f26cb615b3664a1
Dheeraj2907 <dheerajsharma.cbse@gmail.com>
2025-04-07 22:45:56 +0530

    changes

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ function valid(e, i, j, BoxNo) {
   document.getElementById(`${i}${j}`).style.background = "white";
   let IsCorrectCol = (IsCorrectRow = IsCorrectBox = false);
   let num = e.target.value;
-  if(num!="0"&&num.charCodeAt(0)<58&&num.charCodeAt(0)>48){
+  if (num != "0" && num.charCodeAt(0) < 58 && num.charCodeAt(0) > 48) {
     IsCorrectRow = checkRow(i, j, num);
     if (!IsCorrectRow) {
       IsCorrectCol = checkCol(i, j, num);
     }
-    if(!IsCorrectCol){
-        IsCorrectBox = Checkbox(i, j, num);
+    if (!IsCorrectCol) {
+      IsCorrectBox = Checkbox(i, j, num);
     }
     if (!IsCorrectCol && !IsCorrectRow && !IsCorrectBox) {
       SudokuArray[i][j] = num;
     } else {
       document.getElementById(`${i}${j}`).style.background = "red";
     }
-}else{
-    document.getElementById(`${i}${j}`).value='';
-}
+  } else {
+    document.getElementById(`${i}${j}`).value = "";
+  }
 }
 function checkRow(row, col, num) {
   for (let i = 0; i < 9; i++) {
@@ -46,39 +46,39 @@ function checkCol(row, col, num) {
   }
   return false;
 }
-function Checkbox(row, col, num){
-    let RowStart = GiveStart(row);
-    let ColStart= GiveStart(col);
-    for(let i=RowStart;i<RowStart+3;i++){
-        for(let j=ColStart;j<ColStart+3;j++){
-            if(i!=row&&j!=col){
-                if(SudokuArray[i][j]==num){
-                    return true;
-                }
-            }
+function Checkbox(row, col, num) {
+  let RowStart = GiveStart(row);
+  let ColStart = GiveStart(col);
+  for (let i = RowStart; i < RowStart + 3; i++) {
+    for (let j = ColStart; j < ColStart + 3; j++) {
+      if (i != row && j != col) {
+        if (SudokuArray[i][j] == num) {
+          return true;
         }
+      }
     }
-    return false;
+  }
+  return false;
 }
-function GiveStart(a){
-    switch(a){
-        case 0:
-        return 0;
-        case 1:
-        return 0;
-        case 2:
-        return 0;
-        case 3:
-        return 3;
-        case 4:
-        return 3;
-        case 5:
-        return 3;
-        case 6:
-        return 6;
-        case 7:
-        return 6;
-        case 8:
-        return 6;
-    }
+function GiveStart(a) {
+  switch (a) {
+    case 0:
+      return 0;
+    case 1:
+      return 0;
+    case 2:
+      return 0;
+    case 3:
+      return 3;
+    case 4:
+      return 3;
+    case 5:
+      return 3;
+    case 6:
+      return 6;
+    case 7:
+      return 6;
+    case 8:
+      return 6;
+  }
 }
